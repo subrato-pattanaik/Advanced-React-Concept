f16467a150d65056bf32a3a7b18e58dd437e0dcb
subro31 <subrato.pattanaik@gmail.com>
2020-12-08 12:28:16 +0530

    Implemented React Error Boundary

---
+++ src/components/ErrorBoundary/App.js
@@ -0,0 +1,27 @@
+import * as React from 'react'
+import {ErrorBoundary} from 'react-error-boundary'
+
+function ErrorFallback({error}) {
+  return (
+    <div role="alert">
+      <p>Something went wrong:</p>
+      <pre style={{color: 'red'}}>{error.message}</pre>
+    </div>
+  )
+}
+function Greeting({subject}) {
+  return <div>Hello {subject.toUpperCase()}</div>
+}
+function Farewell({subject}) {
+  return <div>Goodbye {subject.toUpperCase()}</div>
+}
+function App() {
+  return (
+    <ErrorBoundary FallbackComponent={ErrorFallback}>
+      <Greeting />
+      <Farewell />
+    </ErrorBoundary>
+  )
+}
+
+export default App
